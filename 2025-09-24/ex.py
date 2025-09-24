1290bb10fe0d42c781d1989dbee8af85857ca9ff
allex-eng <allexvitorgomes@gmail.com>
2025-09-24 18:00:43 -0300

    aula

--- 2025-09-24/ex.py
+++ 2025-09-24/ex.py
@@ -2,8 +2,9 @@ strIP   = '192.168.1.10'
 intCIDR = 24
 
 
-intIP = int.from_bytes(bytes([int(x) for x in strIP.split('.')]) ,'big')
+intIP      = int.from_bytes(bytes([int(x) for x in strIP.split('.')]) ,'big')
 
 intMascara = 0xFFFFFFFF >> (32 - intCIDR) << (32 - intCIDR)
-print(intIP)
-print(intMascara)
+
+intIPRede = intIP & intMascara 
+print(intIPRede)
