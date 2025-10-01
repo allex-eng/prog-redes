d848fbf374a480ef9292aedf47f469d8a34a89f6
allex-eng <allexvitorgomes@gmail.com>
2025-10-01 17:40:52 -0300

    letra

--- 2025 - 10 - 01/bin2.py
+++ 2025 - 10 - 01/bin2.py
@@ -1,11 +1,15 @@
-n1 = 'P'
-n2 = 'd'
+# Operações bit a bit (bitwise) com strings em Python
+strTexto_1 = 'P'
+strTexto_2 = 'd'
 
 
+print(f'Texto 1................: {strTexto_1} -> ASCII = {ord(strTexto_1):>3} -> {ord(strTexto_1):08b}')
+print(f'Texto 2................: {strTexto_2} -> ASCII = {ord(strTexto_2):>3} -> {ord(strTexto_2):08b}')
+# Operação AND bit a bit (&)
+andResultado = ord(strTexto_1) & ord(strTexto_2)
+orResultado  = ord(strTexto_1) | ord(strTexto_2)
+xorResultado = ord(strTexto_1) ^ ord(strTexto_2)
 
-
-print(f"{bin(n1)[2:]}")
-print(f"{bin(n2)[2:]}")
-print(f"\t {n1} & {n2} = {n1 & n2} -> {bin(n1 & n2)[2:]}")
-print(f"\t {n1} | {n2} = {n1 | n2} -> {bin(n1 | n2)[2:]}")
-print(f"\t {n1} ^ {n2} = {n1 ^ n2} -> {bin(n1 ^ n2)[2:]}")
+print(f'strTexto_1 & strTexto_2: ACSII = {andResultado:>3} -> {chr(andResultado)} -> {(andResultado):08b}')
+print(f'strTexto_1 | strTexto_2: ACSII = {orResultado:>3} -> {chr(orResultado)} -> {(orResultado):08b}')
+print(f'strTexto_1 ^ strTexto_2: ACSII = {xorResultado:>3} -> {chr(xorResultado)} -> {(xorResultado):08b}')
