3d4b8fe0412a759645b2be03af4c596c28792ead
allex-eng <allexvitorgomes@gmail.com>
2025-10-01 16:59:14 -0300

    portas

--- 2025 - 09 - 29/at1.py
+++ 2025 - 09 - 29/at1.py
@@ -1,10 +1,13 @@
-strIP   = '192.168.1.10'
+'''
+   Exemplo 03 -  Calculando do endereço de rede a partir de um IP e CIDR
+'''
+strIP   = '192.168.1.255'
 print(f'O Endereço IPv4 é...........................: {strIP}\n')
 
 intCIDR = 24
 print(f'Valor CIDR (bits na máscara)................: /{intCIDR}\n')
 
-intIP      = int.from_bytes(bytes([int(x) for x in strIP.split('.')]), 'big')
+intIP  = int.from_bytes(bytes([int(x) for x in strIP.split('.')]), 'big')
 print(f'O Endereço IPv4 em binário é (32 bits)......: {intIP:032b}\n')  
 
 intMascara = 0xFFFFFFFF >> (32 - intCIDR) << (32 - intCIDR)
---
+++ 2025 - 10 - 01/bin.py
@@ -0,0 +1,8 @@
+n1 = 195
+n2 = 89
+
+print(f"{bin(n1)[2:]}")
+print(f"{bin(n2)[2:]}")
+print(f"\t {n1} & {n2} = {n1 & n2} -> {bin(n1 & n2)[2:]}")
+print(f"\t {n1} | {n2} = {n1 | n2} -> {bin(n1 | n2)[2:]}")
+print(f"\t {n1} ^ {n2} = {n1 ^ n2} -> {bin(n1 ^ n2)[2:]}")
